b45e166a2e3cfd98cec6e1349edf3dfd045a314a
wayne <wayne@HZ30>
2025-05-29 12:51:05 -0600

    Avances en Puestos en el Frontend, agregar, editar y eliminar. Funcionando

--- PFrontend/Models/Puesto.cs
+++ PFrontend/Models/Puesto.cs
@@ -1,17 +1,17 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoNominas.API.Domain.Entities
 {
     public class Puesto
     {
+        [Key]
         public int Id { get; set; }
 
         [Required(ErrorMessage = "El nombre del puesto es obligatorio")]
         [StringLength(100, ErrorMessage = "El nombre no puede tener más de 100 caracteres")]
-        public string Nombre { get; set; } = string.Empty;
+        public string Nombre { get; set; }
 
-        // Relación con empleados
-        public ICollection<Empleado>? Empleados { get; set; }
+        [StringLength(250, ErrorMessage = "La descripción no puede tener más de 250 caracteres")]
+        public string? Descripcion { get; set; }
     }
 }
--- ProyectoNominas.API/Domain/Entities/Empleado.cs
+++ ProyectoNominas.API/Domain/Entities/Empleado.cs
@@ -1,21 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProyectoNominas.API.Domain.Entities
 {
     public class Empleado
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(100)]
         public string Apellido { get; set; } = string.Empty;
+
         public DateTime FechaNacimiento { get; set; }
+
+        [Required]
+        [StringLength(200)]
         public string Direccion { get; set; } = string.Empty;
+
+        [StringLength(30)]
         public string Telefono { get; set; } = string.Empty;
+
+        [StringLength(100)]
         public string Correo { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(20)]
         public string Dpi { get; set; } = string.Empty;
+
         public decimal Salario { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string EstadoLaboral { get; set; } = "Activo";
 
+        // Relaciones
         public int DepartamentoId { get; set; }
         public Departamento? Departamento { get; set; }
 
